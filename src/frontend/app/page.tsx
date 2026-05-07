b7cd1dfa588c54661b5873368256357b7d17a16f
aulianurfajriyah <79237668+aulianurfajriyah@users.noreply.github.com>
2026-05-07 17:56:13 +0700

    test : hello world

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/kota-tua");
+  //redirect("/kota-tua");
+  return <div className="">Hello World</div>
 }
