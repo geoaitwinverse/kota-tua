5ccc917066b07e45e15f5453c8403292824b19cb
aulianurfajriyah <79237668+aulianurfajriyah@users.noreply.github.com>
2026-05-07 15:41:26 +0700

    fix : test deployment

--- src/frontend/app/kota-tua/page.tsx
+++ src/frontend/app/kota-tua/page.tsx
@@ -1,5 +1,4 @@
 import dynamic from "next/dynamic";
-import { ErrorBoundary } from "./ErrorBoundary";
 
 const KotaTuaView = dynamic(() => import("./kotaTuaView"), {
   ssr: false,
@@ -8,9 +7,8 @@ const KotaTuaView = dynamic(() => import("./kotaTuaView"), {
 export default function KotaTuaPage() {
   return (
     <main className="w-full h-screen overflow-hidden">
-      <ErrorBoundary>
-        <KotaTuaView />
-      </ErrorBoundary>
+      //<KotaTuaView />
+      Hello World
     </main>
   );
 }
